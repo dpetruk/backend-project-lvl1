be21b26adea7d2e938753aa96e81bfc2d60f7801
Dmitry Petrouk <dpetrouk@gmail.com>
2020-04-28 21:46:14 +0300

    rename file, refactor getProgression, naming

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,13 +1,9 @@
-import gameLogic from '../index.js';
+import runGameEngine from '../index.js';
 import getRandomInt from '../getRandomInt.js';
 
 const gameRule = 'What number is missing in the progression?';
 
-const progressionLength = 10;
-
-const getProgression = () => {
-  const firstNumber = getRandomInt(0, 100);
-  const diff = getRandomInt(1, 30);
+const getProgression = (firstNumber, diff, progressionLength) => {
   const progression = [];
   for (let i = 0; i < progressionLength; i += 1) {
     progression.push(firstNumber + diff * i);
@@ -16,17 +12,20 @@ const getProgression = () => {
 };
 
 const getGameData = () => {
-  const progression = getProgression();
+  const firstNumber = getRandomInt(0, 100);
+  const diff = getRandomInt(1, 30);
+  const progressionLength = 10;
+  const progression = getProgression(firstNumber, diff, progressionLength);
   const hiddenElementPosition = getRandomInt(0, progressionLength - 1);
 
   const correctAnswer = `${progression[hiddenElementPosition]}`;
 
   progression[hiddenElementPosition] = '..';
-  const questionContent = progression.join(' ');
+  const question = progression.join(' ');
 
-  return [questionContent, correctAnswer];
+  return [question, correctAnswer];
 };
 
-const brainProgression = () => gameLogic(gameRule, getGameData);
+const brainProgression = () => runGameEngine(gameRule, getGameData);
 
 export default brainProgression;
