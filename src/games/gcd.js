a4f4ba7a09a5cf99643caca64b5286c9d9840500
Dmitry Petrouk <dpetrouk@gmail.com>
2020-04-28 21:44:21 +0300

    refactor findGCD, naming

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,25 +1,29 @@
-import gameLogic from '../index.js';
+import runGameEngine from '../index.js';
 import getRandomInt from '../getRandomInt.js';
 
 const gameRule = 'Find the greatest common divisor of given numbers.';
 
-const findGCD = (a, b, div = Math.min(a, b)) => {
-  if (a % div === 0 && b % div === 0) {
-    return div;
+const findGCD = (a, b) => {
+  let div = Math.min(a, b);
+  while (div > 1) {
+    if (a % div === 0 && b % div === 0) {
+      return div;
+    }
+    div -= 1;
   }
-  return findGCD(a, b, div - 1);
+  return 1;
 };
 
 const getGameData = () => {
   const [number1, number2] = [getRandomInt(1, 100), getRandomInt(1, 100)];
 
-  const questionContent = `${number1} ${number2}`;
+  const question = `${number1} ${number2}`;
 
   const correctAnswer = `${findGCD(number1, number2)}`;
 
-  return [questionContent, correctAnswer];
+  return [question, correctAnswer];
 };
 
-const brainGCD = () => gameLogic(gameRule, getGameData);
+const brainGCD = () => runGameEngine(gameRule, getGameData);
 
 export default brainGCD;
