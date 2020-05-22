b2ce65c4241ca7e5a5d2af74c404b1512fd871d4
Dmitry Petrouk <dpetrouk@gmail.com>
2020-05-22 15:09:47 +0300

    optimize isPrime algorithm

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,8 +5,7 @@ const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 const isPrime = (number) => {
   if (number < 2) return false;
-  const maxDivisorToCheck = Math.floor(number / 2);
-  for (let i = 2; i <= maxDivisorToCheck; i += 1) {
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) return false;
   }
   return true;
